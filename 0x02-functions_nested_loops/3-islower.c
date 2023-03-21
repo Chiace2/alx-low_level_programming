a27b0f975b3daee606187c351a06c72ed4acee27
Chiace2 <Chiomaopara97@gmail.com>
2023-03-21 07:21:45 -0700

    return 1

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 /**
- * _islower - print lowercase characters
+ * _islower - print 1 lowercase characters. other cases 0
  * @c: The character to print
  *
- * Return:
+ * Return: 1 for lowercase characters. 0 others
  */
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
+{
 return (1);
+}
 else
+{
 return (0);
+}
+_putchar('\n');
 }
