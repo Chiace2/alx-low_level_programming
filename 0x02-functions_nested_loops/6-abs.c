71bcbcc9cc07f4d8123c1aea8d269361d2caa590
Chiace2 <Chiomaopara97@gmail.com>
2023-03-21 15:27:52 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+* _abs - check the code.
+* @n: The input to number as an integer.
+*
+* Return: n if true. -n if false
+*/
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
