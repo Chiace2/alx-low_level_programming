00c34d6709781833351161b680811e9e225eb2dc
Chiace2 <Chiomaopara97@gmail.com>
2023-03-23 12:02:32 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,29 +8,27 @@
  */
 void print_triangle(int size)
 {
-if (size <= 0)
-{
-_putchar('\n');
-}
-else
-{
-int i, j;
+int i = 1, ii;
 
-for (i = 1; i <= size; i++)
+while (i <= size - i)
 {
-for (j = i; j < size; j++)
+ii = 0;
+while (ii < size - i)
 {
 _putchar(' ');
+ii++;
 }
-
-for (j = 1; j <= i; j++)
+ii = 0;
+while (ii < i)
 {
 _putchar('#');
+ii++;
 }
 
 _putchar('\n');
+ii++;
 }
+if (i == 1)
+_putchar('\n');
 }
-}
-
 
