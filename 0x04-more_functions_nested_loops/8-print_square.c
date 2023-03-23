0ff4ffe2b1de8bb99bb4d626b369660c558b4693
Chiace2 <Chiomaopara97@gmail.com>
2023-03-23 11:31:11 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,11 +14,11 @@ _putchar('\n');
 }
 else
 {
-int k, j;
+int i, j;
 
-for (k = 0; k <= size; k++)
+for (i = 0; i < size; i++)
 {
-for (j = 0; j <= k; j++)
+for (j = 0; j < size; j++)
 {
 _putchar('#');
 }
