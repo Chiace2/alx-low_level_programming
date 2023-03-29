b76d5fe11c7ccb22fa5bfc81886d5bef6973ab43
Chiace2 <Chiomaopara97@gmail.com>
2023-03-29 11:43:02 -0700

    tas6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * *cap_string - checks code to capitalize cases
+ * @str: parameter
+ *
+ * Return: capitalized words
+ */
+char *cap_string(char *str)
+{
+int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+	index++;
+	}
+
+	return (str);
+}
+
+
