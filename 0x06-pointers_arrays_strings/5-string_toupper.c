9bfbe61d7f1117468bf452d443a507aa0835b16b
Chiace2 <Chiomaopara97@gmail.com>
2023-03-29 10:33:26 -0700

    task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,4 +1,21 @@
 #include "main.h"
 
 /**
- * *string_toupper
+ * *string_toupper - checks code to change cases
+ * @d: parameter
+ *
+ * Return: cases
+ */
+char *string_toupper(char *d)
+{
+int i;
+
+for (i = 0; d[i] != '\0'; i++)
+{
+if (d[i] >= 'a' && i <= 'z')
+{
+d[i] = d[i] - 32;
+}
+}
+return (d);
+}
