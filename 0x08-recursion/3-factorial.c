a449aa3cca537a57ddcf6b030b193e6717c4b38b
Chiace2 <Chiomaopara97@gmail.com>
2023-04-05 07:38:52 -0700

    task3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,14 +3,17 @@
 /**
  * factorial - checks code to return the factorial of a number
  * @n: number to return the factorial from
- *
+ * 
  * Return: factorial of n
  */
 int factorial(int n)
 {
+	int fact;
+
 	if (n < 0)
 		return (-1);
 	if (n == 0)
 		return (1);
-	return (n * factorial(n - 1));
+	fact = n * factorial(n - 1);
+	return (fact);
 }
