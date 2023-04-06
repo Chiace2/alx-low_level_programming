41b6d7815e9b15a938633388aad59c9cbc91d06b
Chiace2 <Chiomaopara97@gmail.com>
2023-04-06 07:10:32 -0700

    task 0

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+		
+#define MAIN_H
+		
+
+		
+int _putchar(char c);
+int main(int argc, char *argv[]);		
+int _atoi(char *s);
+		
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+		
+#endif
+
