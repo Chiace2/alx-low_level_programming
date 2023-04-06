687cff7762b46eadb56aa955e5d27d1e406f213c
Chiace2 <Chiomaopara97@gmail.com>
2023-04-06 11:16:14 -0700

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return:  0 (Success), 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!atoi(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				sum = sum + atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
